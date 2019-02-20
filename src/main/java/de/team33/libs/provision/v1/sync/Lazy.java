2e0fbdeee98dd6cd97908085b9dcab16fa7c9b83
KlugeKaindl <akk-gthb@team33.de>
2019-02-20 17:21:24 +0100

    added some java doc

--- src/main/java/de/team33/libs/provision/v1/sync/Lazy.java
+++ src/main/java/de/team33/libs/provision/v1/sync/Lazy.java
@@ -9,10 +9,24 @@ public class Lazy<T> implements Supplier<T> {
 
     private Supplier<T> backing;
 
+    /**
+     * Initializes a new instance based on a supplier describing how the represented value will be determined.
+     */
     public Lazy(final Supplier<T> initial) {
         this.backing = new Initial(initial);
     }
 
+    /**
+     * Returns the value that was determined using the
+     * {@linkplain #Lazy(Supplier) initially specified supplier}.
+     *
+     * Ideally, the result is always the same, once determined value.
+     *
+     * However, if multiple concurrent calls from different threads are executed in parallel, it may happen
+     * that the {@linkplain #Lazy(Supplier) initially specified supplier} is called multiple times before one
+     * of the results is effectively available for further calls. In this case, the result remains
+     * consistent, as long as the results of the supplier are consistent.
+     */
     @Override
     public T get() {
         return backing.get();

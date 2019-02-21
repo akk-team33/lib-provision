8db8eca30b7ac255803fa35ee04ec3a1618cacbc
KlugeKaindl <akk-gthb@team33.de>
2019-02-21 09:49:30 +0100

    updated some java doc on Lazy

--- src/main/java/de/team33/libs/provision/v1/sync/Lazy.java
+++ src/main/java/de/team33/libs/provision/v1/sync/Lazy.java
@@ -3,7 +3,13 @@ package de.team33.libs.provision.v1.sync;
 import java.util.function.Supplier;
 
 /**
- * Implements a supplier that provides a fixed value that is initialized as late as possible.
+ * <p>Implements a supplier that provides a fixed value that is determined once but as late as possible,
+ * and only if needed at all.</p>
+ *
+ * <p>This implementation ensures that the {@linkplain #Lazy(Supplier) initially specified supplier}
+ * is called up to a maximum of once, even for concurrent accesses.</p>
+ *
+ * @see de.team33.libs.provision.v1.Lazy
  */
 public class Lazy<T> implements Supplier<T> {
 
@@ -17,15 +23,17 @@ public class Lazy<T> implements Supplier<T> {
     }
 
     /**
-     * Returns the value that was determined using the
-     * {@linkplain #Lazy(Supplier) initially specified supplier}.
+     * <p>Returns the value that has been determined using the
+     * {@linkplain #Lazy(Supplier) initially specified supplier}.</p>
      *
-     * Ideally, the result is always the same, once determined value.
+     * <p>Ideally, the result is always the same, once determined value.
+     * However, if multiple concurrent calls from different threads are executed in parallel, it may
+     * happen that the {@linkplain #Lazy(Supplier) initially specified supplier} is called multiple times
+     * before one of the results is effectively available for further calls. In this case, the result remains
+     * consistent, as long as the results of the supplier are consistent.</p>
      *
-     * However, if multiple concurrent calls from different threads are executed in parallel, it may happen
-     * that the {@linkplain #Lazy(Supplier) initially specified supplier} is called multiple times before one
-     * of the results is effectively available for further calls. In this case, the result remains
-     * consistent, as long as the results of the supplier are consistent.
+     * <p>When it comes to preventing this in any case, the {@linkplain de.team33.libs.provision.v1.sync.Lazy
+     * synchronized lazy implementation} should be used instead.</p>
      */
     @Override
     public T get() {

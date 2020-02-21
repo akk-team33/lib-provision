8ea02a9fe8efe478f5359b2a8f1204795c7d3923
KlugeKaindl <akk-gthb@team33.de>
2020-02-21 11:37:10 +0100

    Added LazyMap v3

--- src/test/java/de/team33/test/provision/v3/LazyMapTest.java
+++ src/test/java/de/team33/test/provision/v3/LazyMapTest.java
@@ -1,24 +1,25 @@
-package de.team33.test.provision.v2;
+package de.team33.test.provision.v3;
 
-import de.team33.libs.provision.v2.LazyMap;
+import de.team33.libs.provision.v3.LazyMap;
+import de.team33.libs.provision.v3.LazyMap.Key;
 import org.junit.Test;
 
 import java.util.Date;
-import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
 public class LazyMapTest {
 
-    private static final Key<Date> DATE0 = LazyMapTest::newDate0;
+    private static final Key<LazyMapTest, Date> DATE0 = LazyMapTest::newDate0;
 
     private final LazyMap<LazyMapTest> lazy = new LazyMap<>(this);
     private final long time0 = System.currentTimeMillis();
 
     private int time0Count = 0;
 
-    private Date newDate0() {
+    private Date newDate0() throws InterruptedException {
         time0Count += 1;
+        Thread.sleep(1);
         return new Date(time0);
     }
 
@@ -39,7 +40,22 @@ public class LazyMapTest {
         assertEquals(2L, time0Count);
     }
 
-    @FunctionalInterface
-    private interface Key<T> extends Function<LazyMapTest, T> {
+    @Test
+    public void getMultiThreaded() throws InterruptedException {
+        final Thread[] threads = new Thread[500];
+        for (int i = 0; i < threads.length; ++i) {
+            threads[i] = new Thread(() -> lazy.get(DATE0));
+        }
+
+        assertEquals(0, time0Count);
+
+        for (final Thread thread : threads) {
+            thread.start();
+        }
+        for (final Thread thread : threads) {
+            thread.join();
+        }
+
+        assertTrue(1 < time0Count);
     }
 }
